7c76cc4cc3326284cd3a8c7cd9cbada2933712b7
Jean-Baptiste Mérand <jbmerand@tutanota.com>
2019-06-05 15:38:23 +0200

    Wookie-Selfie site - pages de connexion et d'accueil

--- js/script.js
+++ js/script.js
@@ -1,43 +1,67 @@
 'use strict';
 
-// ********** VARIABLES **********
+/*************** FUNCTIONS ***************/
 
-$(document).ready(function () {
-    $.ajax({
-        url: 'https://a38acc3c-84d6-4186-87db-ef6e93d0bce1.mock.pstmn.io/wookies',
-        dataType: 'json', // parses JSON
-        success: function (result) {
-            displayWookieGridInContainer(result);
-        },
-        error: function (error) {
-            console.log("Problème lors de la requête ajax.", error);
-        }
-    })
-});
-
-// ********** FUNCTIONS **********
+function getElementsAndListenForConnexionPage() {
+    const inputSurname = document.getElementById("inputSurname");
+    const inputPassword = document.getElementById("inputSize");
+    const inputBtnConnexion = document.getElementById("btnConnexion");
+    inputBtnConnexion.addEventListener("click", function (event) {
+        event.preventDefault();
+        authenticate(inputSurname, inputPassword);
+    });
+}
 
 function displayWookieGridInContainer(result) {
     let html = '';
     for (let index = 0; index < result.length; index++) {
         let commentaires = '';
         for (let i2 = 0; i2 < result[index].comments.length; i2++) {
-            commentaires += `<p class="m-0">${result[index].comments[i2].wookie.contenu}</p>
+            commentaires += `<p class="m-0">${result[index].comments[i2].content}</p>
                 <small class="m-3">de : ${result[index].comments[i2].wookie.surname}</small>`
         };
         html += `
-            <div class="col"> 
+            <div class="col bg-white m-1 rounded"> 
                 <img 
                     class="img-thumbnail"
-                    style="min-height: 300px;"
                     src = "${result[index].image.url}"
                     alt = "wookie ${result[index].id}">
-                <h5>Commentaires</h5>
+                <h5 class="mt-2 border-top">Commentaires</h5>
                 ${commentaires}
             </div>`;
     }
     $("#container").html(html);
 }
 
+function authenticate(inputSurname, inputPassword) {
+    $(document).ready(function () {
+        var surname = inputSurname.value;
+        var password = inputPassword.value;
+        if(dao.isResponseOk) {
+            let isOK = dao.isAuthentificationOK(surname, password);
+            if(isOK) {
+                // console.log("Authentification réussie.");
+                window.location = "accueil.html";
+            } else {
+                // console.log("Authentification échouée.");
+                let html = `<p class="text-danger text-center m-3">Erreur de connexion. Merci de recommencer.</p>`;
+                $('#divForError').html(html);
+            }
+        } else {
+            console.log("Erreur (API)");
+        }
+    });
+}
 
-// ********** LISTENERS **********
+function getAPIWookiesContent() {
+        $.ajax({
+            url: 'http://ns3055994.ip-193-70-8.eu/selfie-a-wookie-api/api/selfies',
+            dataType: 'json', // parses JSON
+            success: function (result) {
+                displayWookieGridInContainer(result);
+            },
+            error: function (error) {
+                console.log("Problème lors de la requête ajax.", error);
+            }
+        })
+}
--- readme.txt
+++ readme.txt
@@ -29,7 +29,7 @@ gérer l'ajout d'une photo
 Indiquer data-type: 'json' pour l'ajax
 
 es urls à utiliser
-- wookies : https://a38acc3c-84d6-4186-87db-ef6e93d0bce1.mock.pstmn.io/wookies
-- se connecter : https://a38acc3c-84d6-4186-87db-ef6e93d0bce1.mock.pstmn.io/session
+- wookies : http://ns3055994.ip-193-70-8.eu/selfie-a-wookie-api/api/selfies
+- se connecter : http://ns3055994.ip-193-70-8.eu/selfie-a-wookie-api/Api/session
 
 La photo et les commentaires à afficher.
